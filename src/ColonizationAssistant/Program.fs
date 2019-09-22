573374b4e67b15a272947b4c294dcf515640599f
Robert Husák <robert@husak.cloud>
2019-09-22 14:12:31 +0200

    Implement automatic backup of saved game files

--- src/ColonizationAssistant/Program.fs
+++ src/ColonizationAssistant/Program.fs
@@ -1,6 +1,7 @@
 ﻿namespace ColonizationAssistant
 
 open System
+open System.IO
 open Avalonia
 open Avalonia.Logging.Serilog
 
@@ -11,7 +12,12 @@ type Program() =
             .UsePlatformDetect()
             .LogToDebug()
 
-    static let AppMain (app:Application) argv = app.Run(new MainWindow())
+    static let AppMain (app:Application) (argv:string[]) =
+        if argv.Length < 2 || not (Directory.Exists(argv.[0])) || not (Directory.Exists(argv.[1])) then
+            printfn "Expected arguments: [path to COLONIZE folder] [path to backup folder]"
+        else
+            SaveManagement.watchGameSaves argv.[0] argv.[1]
+            app.Run(new MainWindow())
 
     [<EntryPoint>]
     static let Main argv =
---
+++ src/ColonizationAssistant/SaveManagement.fs
@@ -0,0 +1,40 @@
+﻿module ColonizationAssistant.SaveManagement
+
+open System.IO
+
+let backupSavedGame (savePath:string) backupPath =
+    let saveName = Path.GetFileNameWithoutExtension(savePath)
+
+    let lastRevisionPathOpt =
+        let revisions = Directory.GetFiles(backupPath, saveName + ".*.SAV")
+        if revisions.Length = 0 then
+            None
+        else
+            revisions
+            |> Seq.max
+            |> Option.Some
+
+    let nextRevisionNumber =
+        match lastRevisionPathOpt with
+        | Some lastRevisionPath ->
+            Path.GetFileNameWithoutExtension(lastRevisionPath).Split('.').[1]
+            |> int
+            |> (+) 1
+        | None ->
+            0
+    
+    // Solves the problem with FileSystemWatcher - it fires the Changed event twice after a file creation
+    if Option.isNone lastRevisionPathOpt || File.GetLastWriteTime(savePath) <> File.GetLastWriteTime(Option.get lastRevisionPathOpt) then
+        let trgFile = Path.Combine(backupPath, sprintf "%s.%04d.SAV" saveName nextRevisionNumber)
+        File.Copy(savePath, trgFile)
+
+
+let watchGameSaves gamePath backupPath =
+    // TODO: Handle its lifetime (disposal)
+    let watcher = new FileSystemWatcher()
+    watcher.Path <- gamePath
+    watcher.Filter <- "*.SAV"
+    watcher.EnableRaisingEvents <- true
+
+    watcher.Changed.Add(fun e -> backupSavedGame e.FullPath backupPath)
+    ()
